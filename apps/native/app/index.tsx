f4f1b2d129bec2af2559e70ec7d8b8ce8bb7d8a9
José Núñez <79015124+josiext@users.noreply.github.com>
2024-06-08 15:40:04 -0400

    work with expo-router!!!!

---
+++ apps/native/app/index.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "expo-router";
+import { Button } from "@repo/ui";
+
+export default function Native() {
+  const { navigate } = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Hola desde app JAJAJA</Text>
+
+      <Button onClick={() => alert("Hello")} text="Click me!" />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontWeight: "bold",
+    marginBottom: 20,
+    fontSize: 36,
+  },
+});
